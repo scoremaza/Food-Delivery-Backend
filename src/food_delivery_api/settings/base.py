5c9d8cde41d32435df9f62b8a250ebe80f2f5378
Jonathan Constant <jconstantdevry@yahoo.com>
2022-07-19 13:46:49 -0400

    Chore: Configured logging for the django app

--- src/food_delivery_api/settings/base.py
+++ src/food_delivery_api/settings/base.py
@@ -148,3 +148,25 @@ MEDIA_ROOT = str(ROOT_DIR / "mediafiles")
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+CORS_URLS_REGEX = r"^/api/.*$"
+
+LOGGING = {
+    "version": 1,
+    "disable_existing_loggers": False,
+    "formatters": {
+        "verbose": {
+            "format": "%(levelname)s %(name)-12s %(asctime)s %(module)s "
+            "\
+            %(process)d %(thread)d %(message)s"
+        }
+    },
+    "handlers": {
+        "console": {
+            "level": "DEBUG",
+            "class": "logging.StreamHandler",
+            "formatter": "verbose",
+        }
+    },
+    "root": {"level": "INFO", "handlers": ["console"]},
+}
