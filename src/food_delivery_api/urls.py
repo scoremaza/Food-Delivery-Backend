6d543770f1ea7a7b77fc105b75cf796ef22a447b
Jonathan Constant <jconstantdevry@yahoo.com>
2022-07-19 20:48:06 -0400

    Feat: add cutom user model, setup redoc api docs

--- src/food_delivery_api/urls.py
+++ src/food_delivery_api/urls.py
@@ -1,22 +1,27 @@
-"""food_delivery_api URL Configuration
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/3.2/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
 from django.conf import settings
 from django.contrib import admin
 from django.urls import include, path
+from drf_yasg import openapi
+from drf_yasg.views import get_schema_view
+from rest_framework import permissions
 
+schema_view = get_schema_view(
+    openapi.Info(
+        title="Food Delivery API",
+        default_version="v1",
+        description="API endpoints for the Food Delivery API",
+        contact=openapi.Contact(email="admin@enlighten-e.com"),
+        license=openapi.License(name="MIT License"),
+    ),
+    public=True,
+    permission_classes=(permissions.AllowAny,),
+)
 urlpatterns = [
+    path("redoc/", schema_view.with_ui("redoc", cache_timeout=0), name="schema-redoc"),
     path(settings.ADMIN_URL, admin.site.urls),
+    
 ]
+
+admin.site.site_header = " Food Delivery API Admin"
+admin.site.site_title = "Food Delivery API Admin Portal"
+admin.site.index_title = "Welcome to the Food Delivery API Portal"
